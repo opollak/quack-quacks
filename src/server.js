cb4229a63c56cb9c45b950ff5752cd3dcad41ac6
Oren <oreozp@gmail.com>
2022-01-20 13:03:15 -0500

    fixing web start

---
+++ src/server.js
@@ -0,0 +1,27 @@
+//src/server.js
+
+//const Server = require('boardgame.io/server').Server;
+
+import path from 'path';
+import serve from 'koa-static';
+const { Server, Origins } = require('boardgame.io/server');
+const Quacks = require('./Game').Quacks;
+const server = Server({
+  games: [Quacks],
+  origins:[Origins.LOCALHOST_IN_DEVELOPMENT,
+    'https://quack-quacks.herokuapp.com/',
+  'localhost']
+  });
+const PORT = process.env.PORT || 8000;
+
+const frontEndAppBuildPath = path.resolve(__dirname, './build');
+server.app.use(serve(frontEndAppBuildPath))
+
+server.run(PORT, () => {
+  server.app.use(
+    async (ctx, next) => await serve(frontEndAppBuildPath)(
+      Object.assign(ctx, { path: 'index.html' }),
+      next
+    )
+  )
+});
